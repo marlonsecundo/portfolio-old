c3ed388e8706101dfc96617f23ffc5678317a092
Marlon Secundo <marlon_secundo@outlook.com>
2023-10-27 18:20:02 -0300

    Updates

--- src/components/base-layout/styles.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
-
-export const Main = styled(motion.main)`
-  width: 100%;
-  max-width: 100vw;
-  flex-direction: column;
-`;
