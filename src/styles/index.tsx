5b603418b4d7a7b9c3d08c37caa2c18bd108d9bc
Marlon Secundo <marlon_secundo@outlook.com>
2023-12-08 17:30:15 -0300

    clear repo

--- src/styles/index.tsx
+++
@@ -1,5 +0,0 @@
-import media from './mediaquery';
-import metrics from './metrics';
-import colors from './colors';
-
-export { media, metrics, colors };
--- src/styles/metrics.tsx
+++
@@ -1,9 +0,0 @@
-export default {
-  fontFamily: 'Montserrat, sans-serif',
-  fontFamilyJS: 'Neutra Text',
-  aboutFontFamily: 'Financier',
-
-  textSize: '1.1em',
-  subtitleSize: '1.3em',
-  cardTitleSize: '1.9em',
-};
