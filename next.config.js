909d7de839588b6cf313ec5e86e860ff330d452b
Marlon Secundo <marlon_secundo@outlook.com>
2023-12-20 16:20:55 -0300

    fix export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
-
 const debug = process.env.NODE_ENV !== "production";
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: debug ? "" : "/portfolio/",
   output: "export",
