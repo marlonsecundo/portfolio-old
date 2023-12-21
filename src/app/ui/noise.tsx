57bbcb849e3ade8163d4fdde9407817728930147
Marlon Secundo <marlon_secundo@outlook.com>
2023-12-20 23:29:30 -0300

    fix noise image

--- src/app/ui/noise.tsx
+++ src/app/ui/noise.tsx
@@ -1,20 +1,17 @@
-"use client";
-
 import React, { useRef } from "react";
 import { twMerge } from "tailwind-merge";
 
 import noiseImg from "@/assets/images/noise6.png";
+import { BASE_URL } from "../constants/page.constants";
 
 const Noise: React.FC = () => {
-  const divRef = useRef<HTMLDivElement>(null);
-
   return (
     <div className="absolute w-full h-full overflow-clip">
       <div className="w-full h-full relative">
         <div
           style={{
             filter: "brightness(0%)",
-            background: `url(${noiseImg.src})`,
+            background: `url(${BASE_URL + noiseImg.src})`,
           }}
           className={twMerge(
             "absolute inset-0 bg-[200px] bg-repeat opacity-50 pointer-events-none  ",
