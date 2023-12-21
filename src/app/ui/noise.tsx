34ec3a9a605f03b26245897207f2d80b03e8c43f
Marlon Secundo <marlon_secundo@outlook.com>
2023-12-20 23:45:06 -0300

    wip

--- src/app/ui/noise.tsx
+++ src/app/ui/noise.tsx
@@ -13,10 +13,9 @@ const Noise: React.FC = () => {
         <div
           style={{
             filter: "brightness(0%)",
-            background: `url(${noiseUrl})`,
           }}
           className={twMerge(
-            "absolute inset-0 bg-[200px] bg-repeat opacity-50 pointer-events-none  ",
+            "absolute inset-0 bg-[200px] bg-repeat opacity-50 pointer-events-none bg-[url('/portfolio/imgs/noise6.png')]  ",
             "top-[-300%] left-[-150%] h-[600%] w-[600%]",
             "lg:animate-grain"
           )}
